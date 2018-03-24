4277e6fb36d4899b9a13f8441ed6a7e923586344
“anka-wojcik <anka.wojcik@yahoo.com>
2018-03-24 06:51:47 +0100

    Added Steam Exercises - Book, BookDirectory Classes

--- kodlla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodlla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,21 +1,21 @@
 package com.kodilla.stream;
 
-import com.kodilla.stream.beautifier.PoemBeautifier;
-import com.kodilla.stream.iterate.NumbersGenerator;
+import com.kodilla.stream.book.Book;
+import com.kodilla.stream.book.BookDirectory;
+
+import java.util.Map;
+import java.util.stream.Collectors;
 
 
 public class StreamMain {
     public static void main(String[] args) {
-        System.out.println("Welcome to module 7 - Stream\n");
+        BookDirectory theBookDirectory = new BookDirectory();
 
-        PoemBeautifier poemBeautifier = new PoemBeautifier();
-        poemBeautifier.beautify("This text has been changed to be in capital letters.", (text) -> text.toUpperCase() );
-        poemBeautifier.beautify("This text has been changed to have stars before and after.", (text) -> "*** " + text + " ***");
-        poemBeautifier.beautify("This text has been changed to remove all whitespaces.", (text) -> text.replaceAll("\\s+",""));
-        poemBeautifier.beautify("This text has been changed to be wider.", (text) -> text.replaceAll(".", "$0 "));
+        String theResultListOfBooks = theBookDirectory.getList().stream()
+                .filter(book -> book.getYearOfPublication() > 2005)
+                .map(Book::toString)
+                .collect(Collectors.joining(",\n", "<<", ">>"));
 
-
-        System.out.println("\nUsing Stream to generate even numbers from 1 to 20");
-        NumbersGenerator.generateEven(20);
+        System.out.println(theResultListOfBooks);
     }
 }
