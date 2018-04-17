d5e70d58d019a9641431debee6f7e49bc4561aac
“anka-wojcik <anka.wojcik@yahoo.com>
2018-04-17 19:12:11 +0200

    Corrected Test in Portfolio Package

--- kodilla-spring/src/test/java/com/kodilla/spring/portfolio/BoardTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/portfolio/BoardTestSuite.java
@@ -1,8 +1,11 @@
 package com.kodilla.spring.portfolio;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.List;
 
 public class BoardTestSuite {
 
@@ -15,12 +18,22 @@ public class BoardTestSuite {
         String inProgressTask = "task 2";
         String doneTask = "task 3";
 
-        //When & Then
+        //When
         board.addTaskToToDoList(toDoTask);
         board.addTaskToInProgressList(inProgressTask);
         board.addTaskToDoneList(doneTask);
+
+        //Then
         System.out.println("Tasks to do: " + board.getToDoList());
         System.out.println("Tasks in progress: " + board.getInProgressList());
         System.out.println("Tasks done: " + board.getDoneList());
+
+        Assert.assertEquals(1, board.getToDoList().getTasks().size());
+        Assert.assertEquals(1, board.getInProgressList().getTasks().size());
+        Assert.assertEquals(1, board.getDoneList().getTasks().size());
+
+        Assert.assertEquals("task 1", board.getToDoList().getTasks().get(0));
+        Assert.assertEquals("task 2", board.getInProgressList().getTasks().get(0));
+        Assert.assertEquals("task 3", board.getDoneList().getTasks().get(0));
     }
 }
