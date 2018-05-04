dea85bc2c0786158f083d8912a087f13f2157b9b
“anka-wojcik <anka.wojcik@yahoo.com>
2018-05-04 07:16:49 +0200

    Added Invoice-Item-Product Entities ManyToOne Relation - Module 13.3 Task

---
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/invoice/dao/InvoiceDaoTestSuite.java
@@ -0,0 +1,52 @@
+package com.kodilla.hibernate.invoice.dao;
+
+import com.kodilla.hibernate.invoice.Invoice;
+import com.kodilla.hibernate.invoice.Item;
+import com.kodilla.hibernate.invoice.Product;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.math.BigDecimal;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class InvoiceDaoTestSuite {
+    @Autowired
+    private InvoiceDao invoiceDao;
+
+    @Test
+    public void testInvoiceDaoSave() {
+        //Given
+        Invoice invoice = new Invoice("123/F/05/18");
+        Product product1 = new Product("product1");
+        Product product2 = new Product("product2");
+        Item item1 = new Item(product1, new BigDecimal(125), 2);
+        Item item2 = new Item(product1, new BigDecimal(100), 1);
+        Item item3 = new Item(product2, new BigDecimal(5), 10);
+
+        invoice.getItems().add(item1);
+        invoice.getItems().add(item2);
+        invoice.getItems().add(item3);
+        item1.setInvoice(invoice);
+        item2.setInvoice(invoice);
+        item3.setInvoice(invoice);
+
+        item1.setProduct(product1);
+        item2.setProduct(product1);
+        item3.setProduct(product2);
+
+        //When
+        invoiceDao.save(invoice);
+        int id = invoice.getId();
+
+        //Then
+        Assert.assertNotEquals(0, id);
+
+        //CleanUp
+        invoiceDao.delete(id);
+    }
+}
